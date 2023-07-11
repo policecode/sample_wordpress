03d3aa3a284529cac8b5f31ab0b4ef46e9077420
Hoàng Đạt <dat01655993202@gmail.com>
2023-07-12 00:00:57 +0700

    add theme sample wordpress

---
+++ wp-content/themes/sample_themes/functions.php
@@ -0,0 +1,8 @@
+<?php
+// URL
+define ( 'FVN_THEME_URL' , get_stylesheet_directory_uri() );
+
+// DIR
+define ( 'FVN_THEME_DIR' , get_stylesheet_directory() );
+
+// echo get_stylesheet_directory_uri();
